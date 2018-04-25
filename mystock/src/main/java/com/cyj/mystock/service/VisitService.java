35d1e8145288a18fd21ef9cc6858cce4403ae1d9
kennyjin601 <yongjin@163.com>
2018-04-25 16:27:22 +0800

    spring boot 新增

---
+++ mystock/src/main/java/com/cyj/mystock/service/VisitService.java
@@ -0,0 +1,25 @@
+package com.cyj.mystock.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class VisitService {
+    @Autowired
+    private RedisUtil redisUtil;
+    private String key = "visit";
+
+    public void addVisitCount(){
+        boolean exists = redisUtil.exists(key);
+        if(exists){
+            String counts = redisUtil.get(key);
+            Integer count = Integer.valueOf(counts);
+            count = count + 1;
+            redisUtil.set(key,count);
+        }else {
+            redisUtil.set(key,1);
+        }
+    }
+}
--- mystock/src/main/java/com/cyj/mystock/web/PageController.java
+++ mystock/src/main/java/com/cyj/mystock/web/PageController.java
@@ -1,5 +1,7 @@
 package com.cyj.mystock.web;
 
+import com.cyj.mystock.service.VisitService;
+import lombok.extern.log4j.Log4j2;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,19 +12,25 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 
 @CrossOrigin
 @Controller
+@Log4j2
 public class PageController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PageController.class);
 
     @Autowired
     private RestTemplate restTemplate; // HTTP 访问操作类
+    @Autowired
+    private VisitService visitService;
 
     @RequestMapping("/")
-    public String index(Model model) {
+    public String index(Model model,HttpServletRequest request) {
         model.addAttribute("who", "other");
+        visitService.addVisitCount();
+        log.info("访问ip：{}",request.getRemoteAddr());
         return "/index";
     }
     @RequestMapping("/me")
