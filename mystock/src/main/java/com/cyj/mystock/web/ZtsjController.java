3ae6c8fe3c6a5e0ea29221fabfab219966b02c29
13600962259 <chenyongjin@hotmail.com>
2020-03-03 10:26:06 +0800

    docker

--- mystock/src/main/java/com/cyj/mystock/web/ZtsjController.java
+++ mystock/src/main/java/com/cyj/mystock/web/ZtsjController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
+@CrossOrigin
 public class ZtsjController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ZtsjController.class);
 
